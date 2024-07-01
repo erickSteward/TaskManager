97200cdb3d4569101d50dfdfd47c08ab3201c29c
erickSteward <erickmwangimaina8@gmail.com>
2024-07-01 16:55:58 +0300

    Project Structure

--- src/com/taskmanager/Task.java
+++ src/com/taskmanager/Task.java
@@ -1,4 +1,58 @@
 package com.taskmanager;
 
+import java.time.LocalDate;
+
 public class Task {
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+
+    //Constructer
+
+    public Task(String title, String description, LocalDate dueDate, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    //String Override
+
+    @Override
+    public String toString() {
+        return title + " (Due: " + dueDate + ")";
+    }
 }
--- src/com/taskmanager/TaskManagerFrame.java
+++ src/com/taskmanager/TaskManagerFrame.java
@@ -1,4 +1,5 @@
 package com.taskmanager;
 
 public class TaskManagerFrame {
+
 }
