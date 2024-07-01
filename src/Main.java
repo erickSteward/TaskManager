2df2cfadbd9d6310f4e24ac1da20cd1df628ee88
erickSteward <erickmwangimaina8@gmail.com>
2024-07-02 00:45:51 +0300

    Main Refactor

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,5 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            new TaskManagerFrame().setVisible(true);
-        });
     }
 }
--- src/com/taskmanager/Main.java
+++ src/com/taskmanager/Main.java
@@ -1,4 +1,16 @@
 package com.taskmanager;
 
+import javax.swing.*;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            new TaskManagerFrame().setVisible(true);
+        });
+    }
+    }
