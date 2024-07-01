5ee486367369b121fd30c2bfb0c8b960757af0b0
erickSteward <erickmwangimaina8@gmail.com>
2024-07-01 17:57:38 +0300

    So far

--- src/com/taskmanager/TaskManagerFrame.java
+++ src/com/taskmanager/TaskManagerFrame.java
@@ -1,5 +1,51 @@
 package com.taskmanager;
 
-public class TaskManagerFrame {
+import javax.swing.*;
+import java.awt.*;
 
+
+public class TaskManagerFrame extends JFrame{
+    private TaskPanel taskPanel;
+    public TaskManagerFrame (){
+        setTitle("Task Manager");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+
+        initComponents();
+    }
+
+    public void initComponents(){
+        //Create a menu bar
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        exitItem.addActionListener(e -> System.exit(0));
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+
+        //Create the main Content
+        taskPanel = new TaskPanel();
+        add(taskPanel, BorderLayout.CENTER);
+
+        // Create a toolBar
+        JToolBar toolBar = new  JToolBar();
+        JButton addButton = new  JButton("Add Task");
+        addButton.addActionListener(e -> showAddTaskDialog());
+        toolBar.add(addButton);
+
+        add(toolBar, BorderLayout.NORTH);
+
+    }
+
+    public void showAddTaskDialog(){
+        AddTaskDialog dialog = new AddTaskDialog(this);
+        dialog.setVisible(true);
+
+        if (dialog.getTask() != null){
+            taskPanel.addTask(dialog.getTask();
+        }
+    }
 }
--- src/com/taskmanager/TaskPanel.java
+++ src/com/taskmanager/TaskPanel.java
@@ -1,4 +1,68 @@
 package com.taskmanager;
 
-public class TaskPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class TaskPanel extends JPanel {
+    private JList<Task> taskList;
+    private DefaultListModel<Task> listModel;
+    private JTextArea descriptionArea;
+
+    public TaskPanel(JFrame parent){
+        setLayout(new BorderLayout());
+
+        listModel = new DefaultListModel<>();
+        taskList = new JList<>(listModel);
+        taskList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        taskList.addListSelectionListener(e -> updatDescriptionArea());
+
+        JScrollPane listScrollPane = new JScrollPane(taskList);
+        listScrollPane.setPreferredSize(new Dimension(200, 0));
+
+        descriptionArea = new JTextArea();
+        descriptionArea.setEditable(false);
+        JScrollPane descScrollPane = new JScrollPane(descriptionArea);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, listScrollPane, descScrollPane);
+        splitPane.setResizeWeight(0.3);
+
+        add(splitPane, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JButton completeButton = new JButton("Mark Completed");
+        completeButton.addActionListener(e -> markSelectedTaskCompleted());
+        buttonPanel.add(completeButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public void addTask(Task task){
+        listModel.addElement(task);
+    }
+
+    public void updateDescriptionArea(){
+        Task selectedTask = taskList.getSelectedValue();
+        if (selectedTask != null){
+            descriptionArea.setText("Title: " + selectedTask.getTitle() + "\n\n" +
+                    "Description: " + selectedTask.getDescription() + "\n\n" +
+                    "Due Date: " + selectedTask.getDueDate() + "\n\n" +
+                    "Status: " + (selectedTask.isCompleted() ? "Completed" : "Pending"));
+        } else {
+            descriptionArea.setText("");
+        }
+    }
+
+    public void markSelectedTaskCompleted(){
+        int selectedIndex = taskList.getSelectedIndex();
+
+        if(selectedIndex != -1){
+            Task task = listModel.get(selectedIndex);
+            task.setCompleted(true);
+            updateDescriptionArea();
+            taskList.repaint();
+        }
+    }
+
+
+
 }
